7af7ce815bb179cef048a7e82a13edc06c4ce69e
Charlie Sheppard <sheppard.charlie24@gmail.com>
2019-02-19 15:11:49 -0500

    full crud functionality

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -8,4 +8,37 @@ class CreaturesController < ApplicationController
         @creature = Creature.find(params[:id])
     end
 
+    def new
+        @creature = Creature.new
+    end
+
+    def create
+        creature_params = params.require(:creature).permit(:name, :description)
+        creature = Creature.new(creature_params)
+
+        if creature.save
+            redirect_to creature_path(creature)
+        end
+    end
+
+    def edit 
+        @creature = Creature.find(params[:id])
+    end
+
+    def update 
+        creature = Creature.find(params[:id])
+        creature.update_attributes(creature_params)
+        redirect_to creature 
+    end
+
+    def destroy
+        @creature = Creature.destroy(params[:id])
+        redirect_to creatures_path
+    end
+
+    private
+    
+    def creature_params
+        params.require(:creature).permit(:name, :description)
+    end
 end
